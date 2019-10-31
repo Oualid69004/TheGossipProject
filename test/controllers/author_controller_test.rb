5a20257513c00b85c46326e1b22d69ea9aa6e0d1
Oualid69004 <wmersaoui@gmail.com>
2019-11-01 00:00:16 +0100

    Add files via upload

---
+++ test/controllers/author_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AuthorControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get author_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get author_create_url
+    assert_response :success
+  end
+
+end
